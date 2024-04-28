bdda26fa421f7d0a75c48675d5dc0a8b3ce578e4
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-04-28 15:26:01 +0530

    react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import react from 'react'
 import Navbar from "./components/Navbar/Navbar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
 
 const App = () => {
   return(
       <div className='app'>
         <Navbar/>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/placeorder' element={<PlaceOrder/>}/>
+          </Routes>
       </div>
   )
 }
