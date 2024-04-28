00f785a45bce71362c33de2685d4d461b281737c
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-04-28 18:11:36 +0530

    create menu items

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,25 @@
+import react from "react";
+import './ExploreMenu.css'
+import {menu_list} from "../../assets/assets.js";
+
+const ExploreMenu = ({category,setCategory}) => {
+  return(
+      <div className='explore-menu' id='explore-menu'>
+          <h1>Explore Our Menu</h1>
+          <p className='explore-menu-text'>"Discover a diverse range of delicious dishes with our 'Explore Menu' feature. From classics to new favorites, finding your perfect meal is easy and satisfying. Dive in and elevate your food ordering experience today!"</p>
+          <div className="explore-menu-list">
+              {menu_list.map((item,index)=>{
+                  return (
+                      <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=""/>
+                            <p>{item.menu_name}</p>
+                      </div>
+                  )
+              })}
+          </div>
+          <hr/>
+      </div>
+  )
+}
+
+export default ExploreMenu
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import react from "react";
+import react, {useState} from "react";
 import './Home.css'
 import Header from "../../components/Header/Header.jsx";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
 const Home = () => {
+
+    const [category , setCategory] = useState("All");
+
   return(
       <div>
           <Header/>
+          <ExploreMenu category={category} setCategory={setCategory}/>
       </div>
   )
 }
