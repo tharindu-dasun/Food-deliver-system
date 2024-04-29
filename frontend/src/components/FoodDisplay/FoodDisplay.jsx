3d7160bd9836a5c67d6472339b42d90794164645
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-04-29 22:56:19 +0530

    food list update 1

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,8 +11,10 @@ const FoodDisplay = ({category}) => {
       <div className='food-display' id='food-display'>
           <h2>Our Best Dishes For You</h2>
           <div className="food-display-list">
-              {food_list.map(()=>{
-                  return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+              {food_list.map((item,index)=>{
+                  if (category==="All" || category===item.category) {
+                      return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                  }
               })}
           </div>
       </div>
