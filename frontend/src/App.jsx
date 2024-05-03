11047c9cdf6d6e5f2deb4785702869dfc25ea8ba
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-05-03 14:28:04 +0530

    signup component done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import react from 'react'
+import react, {useState} from 'react'
 import Navbar from "./components/Navbar/Navbar.jsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
 
 const App = () => {
+
+    const [showLogin , setShowLogin] = useState(false)
+
   return(
       <>
+          {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
           <div className='app'>
-              <Navbar/>
+              <Navbar setShowLogin={setShowLogin}/>
               <Routes>
                   <Route path='/' element={<Home/>}/>
                   <Route path='/cart' element={<Cart/>}/>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import react, {useState} from "react";
 import './Navbar.css'
 import {assets} from "../../assets/assets.js";
 import {Link} from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu] = useState("home");
 
@@ -21,7 +21,7 @@ const Navbar = () => {
                 <img src={assets.basket_icon} alt=""/>
                 <div className="dot"></div>
             </div>
-          <button>Sign in</button>
+          <button onClick={()=>setShowLogin(true)}>Sign in</button>
       </div>
   )
 }
