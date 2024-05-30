6dcc0517fdb2ba0701cd21a96b4b9d83e3b8cbc6
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-05-30 16:41:27 +0530

    food data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import {connectDB} from "./config/db.js";
+import foodRouter from "./routes/foodRouter.js";
 
 
 //app config
@@ -13,6 +14,9 @@ app.use(cors())
 
 //db connection
 connectDB();
+
+//api endpoint
+app.use("/api/food",foodRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
