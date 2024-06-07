09f902bc7e4ecedcd95c4778207b59e0dc55c1e4
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-06-07 22:54:48 +0530

    start admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+
+const App = () => {
+  return(
+      <div>
+        <Navbar/>
+          <hr/>
+          <div className="app-content">
+              <Sidebar/>
+          </div>
+      </div>
+  )
+}
+
+export default App
