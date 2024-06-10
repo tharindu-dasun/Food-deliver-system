8ada5ec6f4faea0adc460108f6fbe31a3e60c828
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-06-11 00:39:05 +0530

    admin panel update 0.1

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './Navbar.css'
+import {assets} from "../../assets/assets.js";
+
 const Navbar = () => {
     return (
-        <div>
-
+        <div className='Navbar'>
+            <img className='logo' src={assets.logo} alt=""/>
+            <img className='profile' src={assets.profile_image} alt=""/>
         </div>
     )
 }
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import './Sidebar.css'
+import {assets} from "../../assets/assets.js";
 const Sidebar = () => {
   return (
-      <div>
-
+      <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebar-options">
+                <img src={assets.add_icon} alt=""/>
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-options">
+                <img src={assets.order_icon} alt=""/>
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-options">
+                <img src={assets.order_icon} alt=""/>
+                <p>Orders</p>
+            </div>
+        </div>
       </div>
   )
 }
