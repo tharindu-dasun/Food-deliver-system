2500409a5434268b76fac57e8abd03ed3bc807c4
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-06-11 00:58:19 +0530

    admin panel update 0.2

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import Orders from "./pages/Orders/Orders.jsx";
 
 const App = () => {
   return(
@@ -9,6 +13,11 @@ const App = () => {
           <hr/>
           <div className="app-content">
               <Sidebar/>
+              <Routes>
+                  <Route path="/add" element={<Add/>}/>
+                  <Route path="/list" element={<List/>}/>
+                  <Route path="/Orders" element={<Orders/>}/>
+              </Routes>
           </div>
       </div>
   )
