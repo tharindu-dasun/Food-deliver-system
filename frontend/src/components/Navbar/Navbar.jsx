4301f783425a62139164595fa996c743e58bafd0
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-06-14 11:08:40 +0530

    aut update 0.2

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import react, {useContext, useState} from "react";
 import './Navbar.css'
 import {assets} from "../../assets/assets.js";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import storeContext, {StoreContext} from "../Context/StoreContext.jsx";
 const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu] = useState("home");
 
     const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
+
+    const navigate = useNavigate();
+    const logout = () =>{
+        localStorage.removeItem("token");
+        setToken("");
+        navigate("/")
+    }
 
   return(
       <div className='navbar'>
@@ -31,7 +38,7 @@ const Navbar = ({setShowLogin}) => {
                   <ul className="nav-profile-dropdown">
                     <li><img src={assets.bag_icon} alt=""/><p>Orders</p></li>
                       <hr/>
-                      <li><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
+                      <li onClick={logout}><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
                   </ul>
               </div>}
 
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import {createContext, useEffect, useState} from "react";
-import {food_list} from "../assets/assets.js";
+import axios from "axios";
 
 export const StoreContext = createContext(null)
 
@@ -7,6 +7,7 @@ const StoreContextProvider = (props)=>{
     const [cartItems, setCartItems] = useState({});
     const url = "http:/localhost:4000"
     const [token,setToken] = useState("");
+    const [food_List,setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]){
@@ -31,6 +32,22 @@ const StoreContextProvider = (props)=>{
         }
         return totalAmount;
     }
+
+    const fetchFoodList = async ()=> {
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
+    useEffect(()=>{
+
+        async function loadData(){
+            await fetchFoodList();
+            if (localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    },[])
 
     const contextValue = {
         food_list,
